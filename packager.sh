cef8592e6848f3ee5f8bacdd254b6ab4c2e8ab4b
Romain Neutron <imprec@gmail.com>
2013-03-19 14:38:29 +0100

    Remove unnecessary files and exit if one command fails

--- packager.sh
+++ packager.sh
@@ -1,5 +1,29 @@
 git clone git@github.com:alchemy-fr/Phraseanet-Wordpress-Plugin.git
+if [ $? -gt 0 ]; then
+    echo "Error while fetching data from github"
+    exit 1
+fi
+
 curl -s https://getcomposer.org/installer | php
+if [ $? -gt 0 ]; then
+    echo "Error while retrieving composer"
+    exit 1
+fi
+
 mv composer.phar Phraseanet-Wordpress-Plugin
-sh -c "cd Phraseanet-Wordpress-Plugin && php composer.phar install && rm -Rf .git*"
+if [ $? -gt 0 ]; then
+    echo "Error while moving composer.phar to Worpress plugin directory"
+    exit 1
+fi
+
+sh -c "cd Phraseanet-Wordpress-Plugin && php composer.phar install && rm -Rf .git* && rm composer.*"
+if [ $? -gt 0 ]; then
+    echo "Error while initializing dependencies"
+    exit 1
+fi
+
 zip -r -m Phraseanet-Wordpress-Plugin.zip Phraseanet-Wordpress-Plugin
+if [ $? -gt 0 ]; then
+    echo "Error while zipping plugin"
+    exit 1
+fi
